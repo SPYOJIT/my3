dde22ba163bc3f04c5e8c6c49a1aca076ed727c9
SPYOJIT <74659876+SPYOJIT@users.noreply.github.com>
2021-12-14 19:57:31 +0530

    Add files via upload

---
+++ trial/trial.py
@@ -0,0 +1,12 @@
+introString = input("enter String:")
+charecterCount = 0
+wordCount = 1
+
+for i in introString:
+      charecterCount = charecterCount+1
+      if (i == ''):
+            wordCount = wordCount+1
+print("number of word in the String:")  
+print(wordCount)   
+print("number of charecter in the String:")  
+print(charecterCount)           
